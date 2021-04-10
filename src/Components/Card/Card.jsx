2a779da98f23f577c07d511a1ef5c659ff6b8cab
Nader Frigui <nadfri@gmail.com>
2021-04-10 19:42:29 +0200

    ajout bouton installation PWA

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -3,9 +3,11 @@ import "./Card.scss";
 
 function Card({card,click}) {
 
+	const classes = (`back ${card.discover} ${card.wrong} ${card.rotation}`);
+
 	return (
 		<div className="Card">
-			<div className={`back ${card.discover} ${card.wrong} ${card.rotation}`} onClick={click}>
+			<div className={classes} onClick={click}>
 				<img
 					src={card.src}
 					alt=""
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,25 +1,24 @@
-import React from "react";
-import "./Home.scss";
-import LevelLink from "../LevelLink/LevelLink";
-
+import React from 'react';
+import './Home.scss';
+import LevelLink from '../LevelLink/LevelLink';
+import PwaButton from '../PwaButton/PwaButton';
 
 function Home() {
 	return (
-		<div className="Home">
+		<div className='Home'>
 			<h1>React Memory</h1>
-			<div className="levels">
-
-				<LevelLink link="1" content="Niveau 1" cards="8"/>
-				<LevelLink link="2" content="Niveau 2" cards="12"/>
-				<LevelLink link="3" content="Niveau 3" cards="16"/>
-				<LevelLink link="4" content="Niveau 4" cards="20"/>
-				<LevelLink link="5" content="Niveau 5" cards="24"/>
-				<LevelLink link="6" content="Niveau 6" cards="28"/>
-				<LevelLink link="7" content="Niveau 7" cards="32"/>
-				<LevelLink link="8" content="Niveau 8" cards="36"/>
-				<LevelLink link="9" content="Niveau 9" cards="40"/>
-
+			<div className='levels'>
+				<LevelLink link='1' content='Niveau 1' cards='8' />
+				<LevelLink link='2' content='Niveau 2' cards='12' />
+				<LevelLink link='3' content='Niveau 3' cards='16' />
+				<LevelLink link='4' content='Niveau 4' cards='20' />
+				<LevelLink link='5' content='Niveau 5' cards='24' />
+				<LevelLink link='6' content='Niveau 6' cards='28' />
+				<LevelLink link='7' content='Niveau 7' cards='32' />
+				<LevelLink link='8' content='Niveau 8' cards='36' />
+				<LevelLink link='9' content='Niveau 9' cards='40' />
 			</div>
+			<PwaButton />
 		</div>
 	);
 }
