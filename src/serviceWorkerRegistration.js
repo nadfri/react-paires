1995df1745cd1e07a843a3c621351a2ece7c060d
Nader Frigui <nadfri@gmail.com>
2021-11-11 12:18:07 +0100

    Test reload 5sec

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -54,7 +54,7 @@ function registerValidSW(swUrl, config) {
 			setInterval(() => {
 				registration.update();
 				console.log('Checked for update...');
-			}, 1000 * 60 * 5);
+			}, 1000 * 60 * 1);
 
 			registration.onupdatefound = () => {
 				const installingWorker = registration.installing;
@@ -87,8 +87,9 @@ function registerValidSW(swUrl, config) {
 							}
 
 
-							//reload the page
-							window.location.reload();
+							//reload the page in 5 seconds
+							setTimeout(()=>window.location.reload(), 5000);
+							
 
 
 						} else {
