13ec75bfd5aa2a9ddae0d3728fed9ddb9013113e
Nader Frigui <nadfri@gmail.com>
2021-11-14 12:28:18 +0100

    1.0.9

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -51,7 +51,7 @@ function registerValidSW(swUrl, config) {
 			setInterval(() => {
 				registration.update();
 				console.log('Checked for update...');
-			}, 1000 * 60 * 5);
+			}, 1000 * 60 * 1);
 
 			registration.onupdatefound = () => {
 				const installingWorker = registration.installing;
